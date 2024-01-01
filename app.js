31d9c5d744d55b1d92ca0a2051ad2c3f74c29f42
khaing17 <aungthetkhaing017@gmail.com>
2024-01-01 10:27:58 +0630

    crate CRUD : tour

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`).toString()
 );
@@ -17,9 +23,45 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {});
 
 app.get('/', (req, res) => {
   res.status(200).json({
