9172d3fa53883b5043621d06e4eee8cd2398829b
khaing17 <aungthetkhaing017@gmail.com>
2024-03-23 21:01:06 +0630

    feature : monthly plans aggregation

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -148,6 +148,61 @@ const getTourStats = async (req, res) => {
   }
 };
 
+const getMonthlyPlans = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+        },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      total: plan.length,
+      data: plan,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
 module.exports = {
   getAllTours,
   getTour,
@@ -156,4 +211,5 @@ module.exports = {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlans,
 };
--- routes/tour.route.js
+++ routes/tour.route.js
@@ -7,12 +7,14 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlans,
 } = require('../controllers/tour.controller');
 
 const router = express.Router();
 
 // router.param('id');
 router.route('/tours-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlans);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
