6852a36a7d8b342c6ae6e218cb31dd5719343fb5
khaing17 <aungthetkhaing017@gmail.com>
2024-03-30 21:19:02 +0630

    feat : adding node debugging tool ndb

--- model/tour.model.js
+++ model/tour.model.js
@@ -9,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       maxLength: [40, 'A tour name must have less or equal then 40 characters'],
       minLength: [10, 'A tour name must have more or equal then 10 characters'],
+      //this is use with validator library!
       validate: [validator.isAlpha, 'Tour name must only contain Uppercase!'],
     },
     slug: String,
