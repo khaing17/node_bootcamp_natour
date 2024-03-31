d992e7eaada5e03dbfd4d7f9302cd3e39552145c
khaing17 <aungthetkhaing017@gmail.com>
2024-03-31 22:02:24 +0630

    feat : finally this spread operator! whewww!!

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,23 +1,49 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  console.log(err);
+  console.log('Error Value', err.__proto__.value);
+  const message = `Invalid ${err.path}: ${err.__proto__.value}.`;
+  console.log('Message', message);
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const devError = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message,
     error: err,
+    message: err.message,
     stack: err.stack,
   });
 };
 
-const prodError = (err, res) => {
+const prdError = (err, res) => {
+  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+
+    // Programming or other unknown error: don't leak error details
   } else {
-    console.log('Error 💥', err);
+    // 2) Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'Something went wrong!',
+      message: 'Something went very wrong!',
     });
   }
 };
@@ -29,8 +55,14 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     devError(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    prodError(err, res);
-  }
+    let error = Object.create(err);
+    console.log(error.__proto__.name);
 
-  next();
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    prdError(error, res);
+  }
 };
