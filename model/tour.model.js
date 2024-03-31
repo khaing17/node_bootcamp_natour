3169dd939dedad117567d43cf8b69bde9c7d38da
khaing17 <aungthetkhaing017@gmail.com>
2024-03-31 12:43:00 +0630

    feat : error handling in async await

--- model/tour.model.js
+++ model/tour.model.js
@@ -10,7 +10,7 @@ const tourSchema = new mongoose.Schema(
       maxLength: [40, 'A tour name must have less or equal then 40 characters'],
       minLength: [10, 'A tour name must have more or equal then 10 characters'],
       //this is use with validator library!
-      validate: [validator.isAlpha, 'Tour name must only contain Uppercase!'],
+      // validate: [validator.isAlpha, 'Tour name must only contain Uppercase!'],
     },
     slug: String,
     duration: {
