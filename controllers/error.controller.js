2adb32dc2b9c2a1d6545fb6380808d468f062dfd
khaing17 <aungthetkhaing017@gmail.com>
2024-04-05 22:38:49 +0630

    new : handle unhandled exception and rejection

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,16 +1,12 @@
 const AppError = require('./../utils/appError');
 
 const handleCastErrorDB = (err) => {
-  console.log(err);
-  console.log('Error Value', err.__proto__.value);
   const message = `Invalid ${err.path}: ${err.__proto__.value}.`;
-  console.log('Message', message);
   return new AppError(message, 400);
 };
 
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
@@ -31,16 +27,12 @@ const devError = (err, res) => {
 };
 
 const prdError = (err, res) => {
-  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
-
-    // Programming or other unknown error: don't leak error details
   } else {
-    // 2) Send generic message
     res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!',
@@ -56,8 +48,6 @@ module.exports = (err, req, res, next) => {
     devError(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = Object.create(err);
-    console.log(error.__proto__.name);
-
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED EXCEPTION! Shutting down...');
+  process.exit(1);
+});
 const app = require('./app');
 
 const DB = process.env.DB;
@@ -14,6 +20,16 @@ mongoose.connect(DB).then((con) => {
 });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App listening on ${port}`);
 });
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+console.log(x);
