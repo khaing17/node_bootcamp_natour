b5586898ce3b645c4c2ef125d081b51ba4092816
khaing17 <aungthetkhaing017@gmail.com>
2024-04-16 22:32:55 +0630

    feat : implementing authorization logic

--- routes/tour.route.js
+++ routes/tour.route.js
@@ -10,7 +10,7 @@ const {
   getMonthlyPlans,
 } = require('../controllers/tour.controller');
 
-const { protect } = require('../controllers/auth.controller');
+const { protect, restrictTo } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
@@ -19,8 +19,10 @@ router.route('/tours-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlans);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-// User Routes
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
