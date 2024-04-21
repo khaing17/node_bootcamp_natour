8fa0cdf4aa434a1382755f7e1ba9f92c835b24c3
khaing17 <aungthetkhaing017@gmail.com>
2024-04-21 19:19:43 +0630

    feat : prevent paramters pollution

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const { rateLimit } = require('express-rate-limit');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const express = require('express');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/error.controller');
@@ -31,8 +34,28 @@ app.use('/api', limiter);
 //Body parser, reading data from body into req.body
 app.use(express.json());
 
+//Data sanitization against NoSql query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'maxGroupSize',
+      'difficulty',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'price',
+    ],
+  })
+);
 
 //Test middleware
 app.use((req, res, next) => {
