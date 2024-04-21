bed830f03513944b2fb8ef550babb64e7a8c1ffa
khaing17 <aungthetkhaing017@gmail.com>
2024-04-21 14:38:50 +0630

    feat : implement heltmet package

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const { rateLimit } = require('express-rate-limit');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const express = require('express');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/error.controller');
 
 const app = express();
 
+console.log('Node Env Variables', process.env.NODE_ENV);
+
+/**
+ * 1) Global Middlewares
+ */
+//Security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 
+//Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 app.use('/api', limiter);
 
-console.log('Node Env Variables', process.env.NODE_ENV);
+//Body parser, reading data from body into req.body
+app.use(express.json());
 
+//Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
